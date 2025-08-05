7c345625dfabb426ec2cc3334a6d67fb716d8fd6
Dimple Bhondekar <143684494+tranquilsoul9@users.noreply.github.com>
2025-08-05 21:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1127,5 +1127,5 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
