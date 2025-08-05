206c6a168cc39a5797f32d365cecb385d1f4f218
Dimple Bhondekar <143684494+tranquilsoul9@users.noreply.github.com>
2025-08-06 00:18:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1126,8 +1126,8 @@ def logout():
     session.pop('shopkeeper_logged_in', None)
     return redirect(url_for('index'))
     
-    if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000)):
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8050)):
     app.run(host="0.0.0.0", port=port)
 #from flask import Flask
 #import os
