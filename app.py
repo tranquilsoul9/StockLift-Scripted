3b782e9b0b8f56d668e6853bb32e76e468127b3b
Dimple Bhondekar <143684494+tranquilsoul9@users.noreply.github.com>
2025-08-05 23:50:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1125,11 +1125,15 @@ def generate_campaign_content():
 def logout():
     session.pop('shopkeeper_logged_in', None)
     return redirect(url_for('index'))
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)):
     app.run(host="0.0.0.0", port=port)
+#from flask import Flask
+#import os
+
+#app = Flask(__name__)
+
+#if __name__ == "__main__":
+ #   port = int(os.environ.get("PORT", 5000))
+  #  app.run(host="0.0.0.0", port=port)
